3ac2e99cb240eedcdf9498870b8f06981cd946db
Dana Bradley <dbradley0909@outlook.com>
2021-12-09 00:01:32 -0500

    Initial commit

---
+++ src/com/pluralsight/organized/Main.java
@@ -0,0 +1,8 @@
+package com.pluralsight.organized;
+//name of class is com.pluralsight.organized.Main;
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.Println("We got Organized!");
+    }
+}
